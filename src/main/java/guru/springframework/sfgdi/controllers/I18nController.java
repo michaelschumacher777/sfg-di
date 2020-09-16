61f5a16f43ba146a807d0b7f4a4e5aa37c15f83b
Michael <michael.j.schumacher777@gmail.com>
2020-09-16 13:57:08 -0500

    Initial commit - plus a controller

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,18 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MyControllers {
+public class I18nController {
 
     private final GreetingService greetingService;
 
-    public MyControllers(GreetingService greetingService) {
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String sayHello() {
-       return greetingService.sayGreeting();
+    public String sayGreeting() {
+        return greetingService.sayGreeting();
     }
 }
